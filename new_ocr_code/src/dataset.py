8cefaedb3f2c1da5f9ddf99be810ce895deada6b
Gaurav <33745822+Gaurav14cs17@users.noreply.github.com>
2025-03-11 19:18:11 +0530

    Update dataset.py

--- new_ocr_code/src/dataset.py
+++ new_ocr_code/src/dataset.py
@@ -3,42 +3,31 @@ import os
 import torch
 from torchvision import transforms
 from torch.utils.data import Dataset
-from src.tokenizer import Tokenizer
-
-img_trans = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485], std=(0.229)),
-])
 
 
-class Lplate_Dataset(Dataset):
-    def __init__(self, images_path=None , width = 400, height = 200,  max_len = 11 ):
+class Container_Dataset(Dataset):
+    def __init__(self, images_path, width=256, height=256, max_len=17):
         self.images_path = images_path
-        self.images_names =  []
-        self.load_images_dataset()
-        #list(os.listdir(self.images_path))
+        self.images_names = [f for f in os.listdir(images_path) if "+" not in f and "#" not in f]
         self.size = len(self.images_names)
         self.n_chars = max_len
         self.chars = list('1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ')
         self.tokenizer = Tokenizer(self.chars)
-        self.first_run = True
         self.image_width_height = (width, height)
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5], std=[0.5]),
+        ])
 
     def __len__(self):
         return self.size
 
-    def load_images_dataset(self):
-        for file_name in os.listdir(self.images_path):
-            if "+" in file_name or "#" in file_name:
-                continue
-            self.images_names.append(file_name)
-
     def __getitem__(self, idx):
         image = Image.open(os.path.join(self.images_path, self.images_names[idx]))
         image = image.resize(self.image_width_height)
-        image = img_trans(image)
+        image = self.transform(image)
         text = self.images_names[idx].split('.')[0]
-        label_encoder = torch.full((self.n_chars + 2,), self.tokenizer.EOS_token, dtype=torch.long)
+        label_encoder = torch.full((self.n_chars + 2,), self.tokenizer.EOS_token, dtype=np.compat.long)
         ts = self.tokenizer.tokenize(text)
         label_encoder[:ts.shape[0]] = torch.tensor(ts)
         return image, label_encoder
