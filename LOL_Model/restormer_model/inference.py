b62db1eab2ac0c195fb1706bbb6b3681dc8ec234
Gaurav <33745822+Gaurav14cs17@users.noreply.github.com>
2025-03-12 02:07:32 +0530

    Update inference.py

--- LOL_Model/restormer_model/inference.py
+++ LOL_Model/restormer_model/inference.py
@@ -1,132 +1,193 @@
 import torch
 import torch.nn as nn
-import numpy as np
-import cv2
-import argparse
-import os.path as osp
-import sys 
-sys.path.append('/dataset/kunzhou/project/low_light_noisy/ECCV2024_LDRM/')
+import torch.nn.functional as F
+import math
 
+class KANLinear(nn.Module):
+    def __init__(
+        self,
+        in_features,
+        out_features,
+        grid_size=5,
+        spline_order=3,
+        scale_noise=0.1,
+        scale_base=1.0,
+        scale_spline=1.0,
+        enable_standalone_scale_spline=True,
+        base_activation=nn.SiLU,
+        grid_eps=0.02,
+        grid_range=[-1, 1],
+    ):
+        super(KANLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.grid_size = grid_size
+        self.spline_order = spline_order
+        self.base_weight = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.spline_weight = nn.Parameter(
+            torch.Tensor(out_features, in_features, grid_size + spline_order)
+        )
+        if enable_standalone_scale_spline:
+            self.spline_scaler = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.scale_noise = scale_noise
+        self.scale_base = scale_base
+        self.scale_spline = scale_spline
+        self.enable_standalone_scale_spline = enable_standalone_scale_spline
+        self.base_activation = base_activation()
+        self.grid_eps = grid_eps
+        self.grid_range = grid_range
+        self.reset_parameters()
 
-from utils.model_opr import load_model
-from torch.utils.data import Dataset, DataLoader
-from PIL import Image
-from torchvision import transforms, utils
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.base_weight, a=math.sqrt(5) * self.scale_base)
+        with torch.no_grad():
+            noise = (
+                (torch.rand(self.grid_size + 1, self.in_features, self.out_features) - 1 / 2
+            ) * self.scale_noise / self.grid_size
+            self.spline_weight.data.copy_(
+                (self.scale_spline if not self.enable_standalone_scale_spline else 1.0)
+                * self.curve2coeff(
+                    self.grid.T[self.spline_order : -self.spline_order],
+                    noise,
+                )
+            )
+            if self.enable_standalone_scale_spline:
+                nn.init.kaiming_uniform_(self.spline_scaler, a=math.sqrt(5) * self.scale_spline)
 
-import torch.optim as optim
-import torch.optim.lr_scheduler as lr_scheduler
-from tqdm import tqdm
-import logging
-from datetime import datetime
-import os
-import math 
-torch.backends.cudnn.enabled = False
-import torchvision
-from dataloader import  NYU_v2_datset
+    def b_splines(self, x: torch.Tensor):
+        assert x.dim() == 2 and x.size(1) == self.in_features
+        grid: torch.Tensor = self.grid
+        x = x.unsqueeze(-1)
+        bases = ((x >= grid[:, :-1]) & (x < grid[:, 1:])).to(x.dtype)
+        for k in range(1, self.spline_order + 1):
+            bases = (
+                (x - grid[:, : -(k + 1)])
+                / (grid[:, k:-1] - grid[:, : -(k + 1)])
+                * bases[:, :, :-1]
+            ) + (
+                (grid[:, k + 1 :] - x)
+                / (grid[:, k + 1 :] - grid[:, 1:(-k)])
+                * bases[:, :, 1:]
+            )
+        return bases.contiguous()
 
-from metrics.common import calculate_ssim
+    def curve2coeff(self, x: torch.Tensor, y: torch.Tensor):
+        assert x.dim() == 2 and x.size(1) == self.in_features
+        assert y.size() == (x.size(0), self.in_features, self.out_features)
+        A = self.b_splines(x).transpose(0, 1)
+        B = y.transpose(0, 1)
+        solution = torch.linalg.lstsq(A, B).solution
+        result = solution.permute(2, 0, 1)
+        return result.contiguous()
 
-from models.restormer_lap import Restormer
-from models.mirnet_lap import MIRNet
-from models.retinexformer_lap import RetinexFormer
-from models.snr_lap import low_light_transformer as SNR
-from models.mirnet_v2_lap import MIRNet_v2
-from models.unet_lap import UNet
-from models.unet import UNet as UNet_Raw
-from utils import model_opr
+    @property
+    def scaled_spline_weight(self):
+        return self.spline_weight * (
+            self.spline_scaler.unsqueeze(-1)
+            if self.enable_standalone_scale_spline
+            else 1.0
+        )
 
+    def forward(self, x: torch.Tensor):
+        assert x.dim() == 2 and x.size(1) == self.in_features
+        base_output = F.linear(self.base_activation(x), self.base_weight)
+        spline_output = F.linear(
+            self.b_splines(x).view(x.size(0), -1),
+            self.scaled_spline_weight.view(self.out_features, -1),
+        )
+        return base_output + spline_output
 
+class Denoiser(nn.Module):
+    def __init__(self, in_dim=20, out_dim=3, dim=31, level=2, num_blocks=[2, 4, 4]):
+        super(Denoiser, self).__init__()
+        self.dim = dim
+        self.level = level
 
-# model_name = 'restormer_lap'
+        # Input projection
+        self.embedding = nn.Conv2d(24, self.dim, 3, 1, 1, bias=False)
 
-# model_name = 'mirnet_lap'
+        # Encoder
+        self.encoder_layers = nn.ModuleList([])
+        dim_level = dim
+        for i in range(level):
+            self.encoder_layers.append(nn.ModuleList([
+                IGAB(
+                    dim=dim_level, num_blocks=num_blocks[i], dim_head=dim, heads=dim_level // dim),
+                nn.Conv2d(dim_level, dim_level * 2, 4, 2, 1, bias=False),
+                nn.Conv2d(dim_level, dim_level * 2, 4, 2, 1, bias=False)
+            ]))
+            dim_level *= 2
 
-# model_name = 'retinexformer_lap'
+        # Bottleneck
+        self.bottleneck = IGAB(
+            dim=dim_level, dim_head=dim, heads=dim_level // dim, num_blocks=num_blocks[-1])
 
-model_name = 'snr_lap'
+        # Decoder with KAN in skip connections
+        self.decoder_layers = nn.ModuleList([])
+        for i in range(level):
+            self.decoder_layers.append(nn.ModuleList([
+                nn.ConvTranspose2d(dim_level, dim_level // 2, stride=2,
+                                   kernel_size=2, padding=0, output_padding=0),
+                nn.Conv2d(dim_level, dim_level // 2, 1, 1, bias=False),
+                KANLinear(in_features=dim_level // 2, out_features=dim_level // 2),  # KAN layer in skip connection
+                IGAB(
+                    dim=dim_level // 2, num_blocks=num_blocks[level - 1 - i], dim_head=dim,
+                    heads=(dim_level // 2) // dim),
+            ]))
+            dim_level //= 2
 
-# model_name = 'mirnet_v2_lap'
+        # Output projection
+        self.mapping = nn.Conv2d(self.dim, 12, 3, 1, 1, bias=False)
 
-model_name = 'unet_lap'
+        # activation function
+        self.lrelu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
+        self.apply(self._init_weights)
 
-# model_name = 'unet_raw'
-split = 'Test'
+        self.criterion = nn.L1Loss(reduction='mean')
+        self.spy_lap = PyLap(4)
 
-result_root = './results/{}_lolv2'.format(model_name)
-if not os.path.exists(result_root): os.mkdir(result_root)
-print(result_root)
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
 
-result_root_split = './results/{}_lolv2/{}'.format(model_name,split)
-if not os.path.exists(result_root_split): os.mkdir(result_root_split)
-print(result_root_split)
+    def forward(self, x, illu_fea):
+        """
+        x:          [b,c,h,w]         x is feature, not image
+        illu_fea:   [b,c,h,w]
+        return out: [b,c,h,w]
+        """
 
-if 'restormer_lap' in model_name:
-    net = Restormer().cuda()
-    model_path = '/dataset/kunzhou/project/low_light_noisy/logs/Restormer_ei/lolv2_ft/models/restormerft_psnr_24.580655994399468.pth'
+        # Embedding
+        fea = self.embedding(x)
 
-if 'mirnet_lap' in model_name:
-    net = MIRNet().cuda()
-    model_path = '/dataset/kunzhou/project/low_light_noisy/logs/MIRNet_en/mirnet_lolv2/models/best_mirnet.pth'
+        # Encoder
+        fea_encoder = []
+        illu_fea_list = []
+        for (IGAB, FeaDownSample, IlluFeaDownsample) in self.encoder_layers:
+            fea = IGAB(fea, illu_fea)  # bchw
+            illu_fea_list.append(illu_fea)
+            fea_encoder.append(fea)
+            fea = FeaDownSample(fea)
+            illu_fea = IlluFeaDownsample(illu_fea)
 
-if 'retinexformer_lap' in model_name:
-    net = RetinexFormer(stage=1,n_feat=40,num_blocks=[1,2,2]).cuda()
-    model_path = '/dataset/kunzhou/project/low_light_noisy/logs/Retinexformer_ei/retinex_lolv2/models/best_retinex_lolv2.pth'
+        # Bottleneck
+        fea = self.bottleneck(fea, illu_fea)
 
-if 'snr_lap' in model_name:
-    net = SNR(nf=64, nframes=5,groups=8, front_RBs=1,back_RBs=1, center=None,
-                                                           predeblur=True, HR_in=True,
-                                                           w_TSA=True).cuda()
-    model_path = '/dataset/kunzhou/project/low_light_noisy/logs/SNR_ei/snr_en_lolv2_ft/models/best_snr_lolv2_ft1.pth'
+        # Decoder with KAN in skip connections
+        for i, (FeaUpSample, Fution, KANLayer, LeWinBlcok) in enumerate(self.decoder_layers):
+            fea = FeaUpSample(fea)
+            skip_connection = fea_encoder[self.level - 1 - i]
+            skip_connection = KANLayer(skip_connection.permute(0, 2, 3, 1).reshape(-1, skip_connection.size(1))).reshape(skip_connection.size())
+            fea = Fution(torch.cat([fea, skip_connection], dim=1))
+            illu_fea = illu_fea_list[self.level - 1 - i]
+            fea = LeWinBlcok(fea, illu_fea)
 
-if 'mirnet_v2_lap' in model_name:
-    net = MIRNet_v2().cuda()
-    model_path = '/dataset/kunzhou/project/low_light_noisy/logs/MIRNetv2/mirnet_lolv2/models/best_mirnetv2.pth'
+        # Mapping
+        out = self.mapping(fea)
 
-if 'unet_lap' in model_name:
-    net = UNet().cuda() 
-    model_path = '/dataset/kunzhou/project/low_light_noisy/logs/Unet_en/unet_lolv2_lap/models/unet_lap_lolv2.pth'
-
-if 'unet_raw' in model_name:
-    net = UNet_Raw().cuda() 
-    model_path = '/dataset/kunzhou/project/low_light_noisy/logs/Unet_en/unet_lolv2/models/best_retinex_mamba_lolv2.pth'
-
-print("g_net have {:.3f}M paramerters in total".format(sum(x.numel() for x in net.parameters())/1000000.0))
-print("backbone have {:.3f}M paramerters in total".format(sum(x.numel() for x in filter(lambda p: p.requires_grad,net.parameters()))/1000000.0))
-
-load_model(net, model_path)
-
-
-# img_path = "/dataset/kunzhou/project/low_light_noisy/lol_dataset/LOL-v2/Real_captured/Train/Low/"
-
-val_dataset = NYU_v2_datset(split = 'test',rank=0)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=8, pin_memory=True)
-
-@torch.no_grad()
-def validate():
-    net.eval()
-
-    idx = 0
-    img_list = []
-    for idx, imgs in enumerate(val_loader):
-        low_img,img_hn,high_img,snr_prior = imgs[0].cuda(), imgs[1].cuda(), imgs[2].cuda(), imgs[3].cuda()
-        
-        if 'snr' not in model_name:
-            out = net(low_img,img_hn)[-1]
-        else:
-            dark = low_img.clone()
-            dark = dark[:, 0:1, :, :] * 0.299 + dark[:, 1:2, :, :] * 0.587 + dark[:, 2:3, :, :] * 0.114
-            light = snr_prior
-            light = light[:, 0:1, :, :] * 0.299 + light[:, 1:2, :, :] * 0.587 + light[:, 2:3, :, :] * 0.114
-            noise = torch.abs(dark - light)
-            mask = torch.div(light, noise + 0.0001)
-            out = net(low_img,img_hn,mask)[-1]
-
-        resore_img_out_fi = out.clamp(0,1).detach().cpu().numpy()[0]
-        resore_img_out_fi = np.transpose(resore_img_out_fi,(1,2,0))
-
-        if True:
-           
-           cv2.imwrite('{}/{}.png'.format(result_root+'/{}'.format(split),str(idx).zfill(5)),resore_img_out_fi[:,:]*255)
-        idx +=1
-
-validate()
+        return out
