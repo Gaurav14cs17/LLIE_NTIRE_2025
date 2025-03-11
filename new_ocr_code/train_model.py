30275fe1593e6761096de2ee4fed3791d78c3100
Gaurav <33745822+Gaurav14cs17@users.noreply.github.com>
2025-03-11 19:18:28 +0530

    Update train_model.py

--- new_ocr_code/train_model.py
+++ new_ocr_code/train_model.py
@@ -3,165 +3,82 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from model.attention_ocr import AttentionOCR
-from src.dataset import Lplate_Dataset
 from tqdm import tqdm
 
 
-
 class Model_Train:
-    def __init__(self):
-        self.img_width = 160
-        self.img_height = 60
-        self.num_classes = 16
-        self.nh = 512
-        self.fload_weight = False
-        self.teacher_forcing_ratio = 0.5
-        self.save_checkpoint_every = 5
-        self.batch_size = 256
+    def __init__(self, train_images_path, test_images_path, checkpoint_dir):
+        self.train_images_path = train_images_path
+        self.test_images_path = test_images_path
+        self.checkpoint_dir = checkpoint_dir
+        self.img_width = 256
+        self.img_height = 256
+        self.num_classes = 38  # 0-9, A-Z, +, #
+        self.hidden_size = 256
+        self.embed_size = 256
+        self.batch_size = 32
         self.lr = 3e-4
-        self.n_epoch = 200
-        self.n_workers = 1
-        self.test_images_path = "./dataset/"
-        self.train_images_path = "./dataset/"
-        self.device = "cpu"
-        self.load_weight = False
+        self.n_epoch = 1000
+        self.n_workers = 4
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.save_checkpoint_every = 30
 
-        print("-----------------Start training----------------------------")
-
-    def load_dataloader(self):
-        ds_train = Lplate_Dataset(self.train_images_path, self.img_width, self.img_height, self.num_classes + 1)
-        ds_test = Lplate_Dataset(self.test_images_path, self.img_width, self.img_height, self.num_classes + 1)
-        self.tokenizer = ds_train.tokenizer
-        self.train_loader = DataLoader(ds_train, batch_size=self.batch_size, shuffle=True, num_workers=self.n_workers)
-        self.test_loader = DataLoader(ds_test, batch_size=self.batch_size, shuffle=False, num_workers=self.n_workers)
-
-    def load_model(self):
-        self.model = AttentionOCR(self.num_classes)
-        self.model.to(device=self.device)
-        self.criterion = nn.NLLLoss().cuda()
-
-        # load_weight_path
-        if self.load_weight:
-            load_weights = torch.load('./inception_v3_google-1a9a5a14.pth')
-            names = set()
-            for k, w in self.model.incept_model.named_children():
-                names.add(k)
-            if self.load_weight:
-                weights = {}
-                for k, w in self.load_weight.items():
-                    if k.split('.')[0] in names:
-                        weights[k] = w
-                self.model.incept_model.load_state_dict(weights)
-
-    def train_batch(self, input_tensor, target_tensor):
-        self.model.train()
-        decoder_output = self.model(input_tensor, target_tensor)
-        loss = 0
-        self.optimizer.zero_grad()
-        for i in range(decoder_output.size(1)):
-            loss += self.criterion(decoder_output[:, i, :].squeeze(), target_tensor[:, i + 1])
-        loss.backward()
-        self.optimizer.step()
-        target_tensor = target_tensor.cpu()
-        decoder_output = decoder_output.cpu()
-        prediction = torch.zeros_like(target_tensor)
-        prediction[:, 0] = self.tokenizer.SOS_token
-        for i in range(decoder_output.size(1)):
-            prediction[:, i + 1] = decoder_output[:, i, :].squeeze().argmax(1)
-        n_right = 0
-        n_right_sentence = 0
-        for i in range(prediction.size(0)):
-            eq = prediction[i, 1:] == target_tensor[i, 1:]
-            n_right += eq.sum().item()
-            n_right_sentence += eq.all().item()
-
-        _avg_loss = loss.item() / len(decoder_output)
-        _acc =  n_right / prediction.size(0) / prediction.size(1)
-        _sentence_acc =  n_right_sentence / prediction.size(0)
-        return _avg_loss,_acc,  _sentence_acc
-
-
-    def predict_batch(self, input_tensor):
-        self.model.eval()
-        decoder_output = self.model(input_tensor)
-        return decoder_output
-
-
-    def eval_batch(self, input_tensor, target_tensor):
-        loss = 0
-        decoder_output = self.predict_batch(input_tensor)
-        for i in range(decoder_output.size(1)):
-            loss += self.criterion(decoder_output[:, i, :], target_tensor[:, i + 1])
-        target_tensor = target_tensor.cpu()
-        decoder_output = decoder_output.cpu()
-        prediction = torch.zeros_like(target_tensor)
-        prediction[:, 0] = self.tokenizer.SOS_token
-        for i in range(decoder_output.size(1)):
-            prediction[:, i + 1] = decoder_output[:, i, :].argmax(1)
-        n_right = 0
-        n_right_sentence = 0
-        for i in range(prediction.size(0)):
-            eq = prediction[i, 1:] == target_tensor[i, 1:]
-            n_right += eq.sum().item()
-            n_right_sentence += eq.all().item()
-
-        _avg_loss = loss.item() / len(decoder_output)
-        _acc = n_right / prediction.size(0) / prediction.size(1)
-        _sentence_acc = n_right_sentence / prediction.size(0)
-        return _avg_loss, _acc, _sentence_acc
-
-
-
-
-    def train_epoch(self):
-        sum_loss_train = 0
-        n_train = 0
-        sum_acc = 0
-        sum_sentence_acc = 0
-        for i, batch in enumerate(tqdm(self.train_loader)):
-            x, y = batch
-            x = x.to(device=self.device)
-            y = y.to(device=self.device)
-            loss, acc, sentence_acc = self.train_batch(x, y)
-            sum_loss_train += loss
-            sum_acc += acc
-            sum_sentence_acc += sentence_acc
-            n_train += 1
-        return sum_loss_train / n_train, sum_acc / n_train, sum_sentence_acc / n_train
-
-    def eval_epoch(self):
-        sum_loss_eval = 0
-        n_eval = 0
-        sum_acc = 0
-        sum_sentence_acc = 0
-        for bi, batch in enumerate(tqdm(self.test_loader)):
-            x, y = batch
-            x = x.to(device=self.device)
-            y = y.to(device=self.device)
-            loss, acc, sentence_acc = self.eval_batch(x, y)
-            sum_loss_eval += loss
-            sum_acc += acc
-            sum_sentence_acc += sentence_acc
-            n_eval += 1
-        return sum_loss_eval / n_eval, sum_acc / n_eval, sum_sentence_acc / n_eval
-
-
-    def run(self):
+        print("Initializing model and data loaders...")
         self.load_dataloader()
         self.load_model()
+
+    def load_dataloader(self):
+        self.ds_train = Container_Dataset(self.train_images_path, self.img_width, self.img_height, self.num_classes)
+        self.ds_test = Container_Dataset(self.test_images_path, self.img_width, self.img_height, self.num_classes)
+        self.tokenizer = self.ds_train.tokenizer
+        self.train_loader = DataLoader(self.ds_train, batch_size=self.batch_size, shuffle=True, num_workers=self.n_workers)
+        self.test_loader = DataLoader(self.ds_test, batch_size=self.batch_size, shuffle=False, num_workers=self.n_workers)
+
+    def load_model(self):
+        self.model = AttentionOCR(self.num_classes, self.hidden_size, self.embed_size).to(self.device)
+        self.criterion = nn.CrossEntropyLoss().to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+
+    def train_epoch(self):
+        self.model.train()
+        total_loss, total_acc, total_sentence_acc = 0, 0, 0
+        for x, y in tqdm(self.train_loader):
+            x, y = x.to(self.device), y.to(self.device)
+            loss, acc, sentence_acc = train_batch(x, y, self.model, self.optimizer, self.criterion, self.tokenizer)
+            total_loss += loss
+            total_acc += acc
+            total_sentence_acc += sentence_acc
+        return total_loss / len(self.train_loader), total_acc / len(self.train_loader), total_sentence_acc / len(self.train_loader)
+
+    def eval_epoch(self):
+        self.model.eval()
+        total_loss, total_acc, total_sentence_acc = 0, 0, 0
+        for x, y in tqdm(self.test_loader):
+            x, y = x.to(self.device), y.to(self.device)
+            loss, acc, sentence_acc = eval_batch(x, y, self.model, self.criterion, self.tokenizer)
+            total_loss += loss
+            total_acc += acc
+            total_sentence_acc += sentence_acc
+        return total_loss / len(self.test_loader), total_acc / len(self.test_loader), total_sentence_acc / len(self.test_loader)
+
+    def run(self):
         for epoch in range(self.n_epoch):
             train_loss, train_acc, train_sentence_acc = self.train_epoch()
             eval_loss, eval_acc, eval_sentence_acc = self.eval_epoch()
-            print("Epoch %d" % epoch)
-            print('train_loss: %.4f, train_acc: %.4f, train_sentence: %.4f' % (train_loss, train_acc, train_sentence_acc))
-            print('eval_loss:  %.4f, eval_acc:  %.4f, eval_sentence:  %.4f' % (eval_loss, eval_acc, eval_sentence_acc))
-            if epoch % self.save_checkpoint_every == 0 and epoch > 0:
-                print('saving checkpoint...')
-                torch.save(self.model.state_dict(),'./chkpoint/time_%s_epoch_%s.pth' % (time.strftime('%Y-%m-%d_%H-%M-%S'), epoch))
+            if (epoch + 1) % 10 == 0:
+              print(f"Epoch {epoch + 1}/{self.n_epoch}")
+              print(f"Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f}, Train Sentence Acc: {train_sentence_acc:.4f}")
+              print(f"Eval Loss: {eval_loss:.4f}, Eval Acc: {eval_acc:.4f}, Eval Sentence Acc: {eval_sentence_acc:.4f}")
+
+            if (epoch + 1) % self.save_checkpoint_every == 0:
+                checkpoint_path = f"{self.checkpoint_dir}/checkpoint_epoch_{epoch + 1}.pth"
+                torch.save(self.model.state_dict(), checkpoint_path)
+                print(f"Checkpoint saved at {checkpoint_path}")
 
 
 if __name__ == '__main__':
-    model_train_obj = Model_Train()
-    model_train_obj.run()
+    train_images_path = "/content/drive/MyDrive/OCR_March/resized_images"
+    test_images_path = "/content/drive/MyDrive/OCR_March/test"
+    checkpoint_dir = "/content/drive/MyDrive/OCR_March/chkpoint"
+    model_train = Model_Train(train_images_path, test_images_path, checkpoint_dir)
+    model_train.run()
